91f55db02af250e8500bad637ebb680e5e34049b
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-08 15:44:49 -0400

    Attempt ex. 2 - fail

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let i = 0
+    let multiString = string
 
+    while (i < num) {
+        let multiString = string
+        multiString = (multiString + string);
+        i++;
+    }
+
+    return(multiString)
 };
+
+repeatString("hey", 3)
 
 // Do not edit below this line
 module.exports = repeatString;

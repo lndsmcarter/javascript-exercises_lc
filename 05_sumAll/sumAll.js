8cff3a6c809da3e53632ae20f75e4c4f021da4f6
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-09 13:57:17 -0400

    Solve ex. 5 and pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let newVal = 0;
+    if (num1 > num2) {
+        let extVar = num1
+        num1 = num2
+        num2 = extVar
+    }
+    else if ((num1 < 0) ||
+        (Number.isInteger(num1) == false) || 
+        (Number.isInteger(num2) == false)) {
+        return ("ERROR")
+    }
 
+    for (i = num1; i <= num2; i++) {
+        newVal = newVal + i;
+    }
+    
+    return(newVal);
 };
 
 // Do not edit below this line

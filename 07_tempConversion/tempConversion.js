2bc6d8ee60d1cf977b185bc353bbaafe19c4c92c
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-09 14:42:52 -0400

    Solve ex. 7 and pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(far) {
+  let tempCels = (far - 32) * (5/9);
+  let newCels = (Math.round(tempCels * 10)) / 10
+  return newCels;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let tempFar = ((9/5) * cels) + 32;
+  let newFar = (Math.round(tempFar * 10)) / 10
+  return newFar;
 };
 
 // Do not edit below this line

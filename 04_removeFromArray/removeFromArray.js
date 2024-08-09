2302de7a89d0f72596c2bc673b1dd134a11f4f4a
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-09 11:29:23 -0400

    Write removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, int) {
+   
+    let newArray = []
+
+    for (item of array) {
+        i = 1;
+        if (item == arguments[i]) continue;
+        else {
+            newArray.push(item);
+        }
+        i++;
+    }
+    return newArray 
 };
 
 // Do not edit below this line

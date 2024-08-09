8c44518bf2c2a666b2c185a9add1c8c5fc1cef0c
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-09 13:18:50 -0400

    Solve ex. 4 (ish)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,21 +1,40 @@
-const removeFromArray = function(array,...args) {
+const removeFromArray = function(array, ...args) {
     
-    let newArray = []
-
-    for (item of array) {
-        for(let i = 1; i <= arguments.length; i++) {
-            if (item != arguments[i])
-                continue;
-            else if (item == arguments[i])
-                return false;
-            else {
-                newArray.push(item);
-                return true;
-            }
+    let newArray = [];
+    
+    array.forEach((element) => {
+        if(!args.includes(element)) {
+            newArray.push(element);
         }
-    }
-    return newArray 
-};
+        });
+
+
+    return newArray;
+}
+        
+        // for(let i = 1; i <= args.length; i++) {
+        //     if (element == args[i])
+        //         return "";
+        //     else {
+                
+        //     }
+        // }
+    // });
+    
+    // for (item of array) {
+    //     for(let i = 1; i <= arguments.length; i++) {
+    //         if (item != arguments[i])
+    //             continue;
+    //         else if (item == arguments[i])
+    //             return false;
+    //         else {
+    //             newArray.push(item);
+    //             return true;
+    //         }
+    //     }
+    // }
+    // return newArray 
+
 
 // Do not edit below this line
 module.exports = removeFromArray;

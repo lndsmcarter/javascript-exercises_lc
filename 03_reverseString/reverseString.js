934eb3de61e46b757daa75567bf0d24b3e71a3f4
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-09 10:54:52 -0400

    Solve ex. 3 and pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+//Find the length of string and store it in vairalbe n
+let lenString = string.length;
+let newString = "";
+//Create a loop
+for (i = 1; i <= lenString; i++) {
+    let char = string[lenString - i];
+    newString += char;
+}
 
+return(newString);
 };
 
 // Do not edit below this line

27c39cf6c8f7a618c9b035cb0aee472a8e3bf0b8
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-09 09:26:23 -0400

    Run and pass tests - still working on test 5

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,14 +1,23 @@
 const repeatString = function(string, num) {
     let multiString = string;
 
-    for (let i = 0; i < num; i++) {
-        if (i == 0) {
+    for (let i = 0; i <= num; i++) {
+        if (num < 0) {
+            multiString = "ERROR";
+            break;
+        }
+        else if (i == 0) {
+            multiString = "";
+        }
+        else if (i == 1) {
             multiString = string;
         }
         else {
             multiString = multiString + string;
         };
     }
+    
+    
     return (multiString);
 }
 

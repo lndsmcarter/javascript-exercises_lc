9a93ea3aa55c18e58a822561a128bf524a635378
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-31 13:16:55 -0400

    Finish ex. 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    let array = [1, 1];
+    x = Number(x)
+    for (let i = 1; i < x; i++) {
+        let num = (array.at(-2) + array.at(-1));
+        array.push(num);
+    }
 
+    if (x < 0) {
+        return "OOPS";
+    } else if (x == 0) {
+        return x;
+    } else {
+        return array.at(x - 1);
+    };
 };
 
 // Do not edit below this line

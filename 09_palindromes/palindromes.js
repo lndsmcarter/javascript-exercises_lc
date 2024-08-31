8273206e4dba3b00420a53bf32ba42e5e88f35c9
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-30 22:42:09 -0400

    Start ex 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (arr) {
+    let forwards = arr.split("" && ", ")
+    let backwards = forwards.reverse();
 
+    if (backwards == forwards) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line

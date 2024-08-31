1479df8603c8812765516f1694d7a06e00ec9601
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-31 14:42:21 -0400

    Finish ex 12 except for the undefined death date ones

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = (new Date()).getFullYear();
 
+    
+
+    let oldest = people.sort(function(a, b) {
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    }) 
+    
+    return oldest[0]
 };
 
 // Do not edit below this line

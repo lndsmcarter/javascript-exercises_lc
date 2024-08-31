13ffc5a19529442f9ce21ff376a5d8cbea259d50
Linds <94403237+lndsmcarter@users.noreply.github.com>
2024-08-30 21:22:04 -0400

    Complete ex 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, number) => (total + number), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total, number) => (total * number));
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return (a ** b);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let arr = [];
+  for (let i = 1; i <= x; i++) {
+    arr.push(i)
+  };
+  return arr.reduce((total, number) => (total * number), 1);
 };
 
 // Do not edit below this line
